9983605ac6948d64a245bd2dc8c164eaac3aedbe
K1L14N <kyzlow@gmail.com>
2018-09-08 00:13:56 +0200

    creating the grid with basic functionnalities, a form with data binding, props to communicate between components, little validator

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,65 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <Table :table="tableObj"></Table>
+    <div class="form">
+      <input v-model="id" type="text" placeholder="id">
+      <input v-model="word" type="text" placeholder="word">
+      <button @click="onAdd(id, word)">Add</button>
+    </div>
+    
+    <label class="fullWidth" v-if="errorMessage === '' ">You entered {{ id }}: {{ word }}</label>
+    <label class="fullWidth" v-if="errorMessage !== '' && sendOnce">{{ errorMessage }}</label>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Table from "./components/Table.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    Table
+  },
+  data() {
+    return {
+      id: "",
+      word: "",
+      errorMessage: "",
+      tableObj: [],
+      sendOnce: false,
+    };
+  },
+  methods: {
+    onAdd: function(id, word) {
+      this.sendOnce = true
+      var pattern = /^\d+$/;
+      if (!pattern.test(id)) {
+        this.errorMessage = "Invalid id"
+      } else {
+        this.errorMessage = ""
+        this.tableObj.push({id, word})
+      }
+    }
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.form {
+  width: 10px auto;
+  margin-top: 60px;
+  margin-bottom: 5px;
+}
+.fullWidth {
+  width: 10 auto;
 }
 </style>
---
+++ src/components/Table.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="table">
+    <Tile v-for="tile in table" :key=tile.id :word=tile.word :id=tile.id></Tile>
+  </div>
+</template>
+
+<script>
+import Tile from './Tile.vue'
+
+export default {
+  name: "Table",
+  components: {
+    Tile
+  },
+  props: {
+    table: [],
+  },
+  data() {
+    return {
+      // tiles: [
+      //   {id: 0, word: "sceau"},
+      //   {id: 1, word: "dés"},
+      // ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+.table {
+  display: grid;
+  grid-template-columns: repeat(10, 1fr);
+  grid-auto-rows: minmax(50px, auto);
+  background-color: rgba(134, 187, 134, 0.872);
+}
+</style>
